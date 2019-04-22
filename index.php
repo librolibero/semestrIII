feb3fdc0dd5f9ad5e97123985e17bde38d0b49d9
librolibero <37860160+librolibero@users.noreply.github.com>
2019-04-22 13:44:36 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <link rel="shortcut icon" href="images/128.gif">
+    <meta charset="UTF-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>
+    <title>Książka tel</title>
+    <link rel='stylesheet' href='css/mojestyle.css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center;}
+        .wrapper{ width: 350px; padding: 20px; }
+        body {
+            padding-top: 40px;
+            padding-bottom: 40px;
+            background-image: url(images/128.gif);
+         }
+         
+ 
+       
+.form-group{
+    width: 50%;
+}
+         .container {
+    margin: 30px  auto;
+     border-top-width: 15px;}   
+         
+          h1{
+            text-align: center;
+            color: #FFFFFF;
+         }
+         
+         h2{
+            text-align: center;
+            color: #4B0082;
+         }
+         h4{
+            text-align: center;
+            color: white;
+
+         }
+ </style>
+          
+   
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <!-- header -->
+        <header>
+            <img src="images/header.jpg" alt="" />
+        </header>
+
+        <!-- sidebar -->
+        <aside>
+            <nav>
+
+                <div class="mobimin" onclick="$('.menu').toggle();">
+                    &equiv;
+                </div>
+                <ul class="menu">
+                    <li><a href="index.php">Home</a></li>
+                 <li><a href="login.php">Zaloguj się</a></li>
+                 <li><a href="register.php">Zarejestruj się</a></li>
+                 <li><a href="uploadd.php">Dodaj zdjęcie</a></li>
+                 <li><a href="reset-password.php">Zresetuj hasło</a></li>
+                    
+                </ul>
+            </nav>
+        </aside>
+        <!-- main -->
+        <section id="main">
+            <h1>Home</h1>
+            
+            <h1>Książka telefoniczna .</h1>
+        </section>
+
+        <!-- footer -->
+        <footer>
+            <p>Humanistyka UKW © 2019</p>
+        </footer>
+     
+</body>
+
+</html>
