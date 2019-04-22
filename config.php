5381e96edb83f940353b63d8ad66d859c507088d
librolibero <37860160+librolibero@users.noreply.github.com>
2019-04-22 13:45:13 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'vertrigo');
+define('DB_NAME', 'xyz');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Nie mozna sie połączyć. " . mysqli_connect_error());
+}
+?>
